d4f9f71013f6318ba48a57468430db6d4df8c0b9
Steve Cortes Rodriguez <steve@MacBook-Pro-de-Steve.local>
2016-05-15 22:45:03 -0500

    editar entidad agregado

--- controllers/entidadController.js
+++ controllers/entidadController.js
@@ -2,7 +2,7 @@
 * @Author: steve
 * @Date:   2016-04-06 16:15:38
 * @Last Modified by:   steve
-* @Last Modified time: 2016-05-11 23:13:25
+* @Last Modified time: 2016-05-15 22:35:11
 */
 
 (function() {
@@ -24,6 +24,24 @@ function CrearEntidadCtrl ($scope, $uibModalInstance, $state, Entidad) {
     };
 };
 
+function EditarEntidadCtrl ($scope, $uibModalInstance, $state, Entidad, ID) {
+    $scope.entidad = Entidad.get({id_entidad:ID});
+    $scope.pform = {'id':ID};
+
+    $scope.sendForm = function(){
+        if($scope.mainForm.$valid){
+            $object = Entidad.update($scope.pform, function(){
+                $uibModalInstance.close();
+                $state.go("entidades.lista", null, {reload: true});
+            });         
+        }
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+};
+
 var entidadController = function($scope, $state, $log, $uibModal, auth, Entidad) {
 
     $log.info('---- entidadController ----- ');
@@ -32,6 +50,18 @@ var entidadController = function($scope, $state, $log, $uibModal, auth, Entidad)
         var modalInstance = $uibModal.open({
             templateUrl: 'views/entidades/modals/crear.html',
             controller: CrearEntidadCtrl
+        });
+    };
+
+    $scope.editar = function (id) {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/entidades/modals/editar.html',
+            controller: EditarEntidadCtrl,
+            resolve: {
+                ID: function () {
+                  return id;
+                }
+            }
         });
     };
 
--- models/models.js
+++ models/models.js
@@ -2,7 +2,7 @@
 * @Author: steve
 * @Date:   2016-04-11 16:19:44
 * @Last Modified by:   steve
-* @Last Modified time: 2016-05-12 00:19:27
+* @Last Modified time: 2016-05-15 22:33:57
 */
 
 (function () {
@@ -10,7 +10,13 @@
 	var baseURL= 'http://rest-api-ivc.mybluemix.net';
 
     angular.module('ivcApp').factory('Entidad', function ($resource, auth) {
-    	return $resource(baseURL+'/api/Entidades/:id_entidad?access_token='+auth.getToken());
+    	return $resource(
+    		baseURL+'/api/Entidades/:id_entidad?access_token='+auth.getToken(),
+    		null,
+    		{
+    			'update' : {method:'PUT'}
+    		}
+    	);
 	});
 
 	angular.module('ivcApp').factory('Dependencia', function ($resource, auth) {
